debdf9ae0a1eeef85814009747083a38ceed8a64
stan <anton.stepanovitch@gmail.com>
2016-03-16 01:05:50 +0300

    mc_sign final

--- 93load/mc_sign.sh
+++ 93load/mc_sign.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+ssh root@bile.test './mt_sign.sh' &
+ssh root@krir.test './mt_sign.sh' &
+ssh root@namo.test './mt_sign.sh' &
+ssh root@tata.test './mt_sign.sh' &
+ssh root@nevl.dev './mt_sign.sh' &
+ssh root@yami.test './mt_sign.sh' &
+ssh root@ws000040.test './mt_sign.sh' &
+ssh root@ilma.test './mt_sign.sh' &
 ./mt_sign.sh &
-ssh root@qa.test './mt_sign.sh' &
+ssh nevl@nevl.test './mt_sign.sh' &
+
 wait
