c3363f10869c131382ddf82733009d85c9a84cfc
Anton Stepanovich <stan@gurtam.com>
2016-03-15 22:59:48 +0300

    comment debug output

--- 93load/sign.sh
+++ 93load/sign.sh
@@ -48,7 +48,7 @@ urlencode() {
     post_data="client_id=${client_id}&redirect_uri=http://${server}/login_simple.html&access_type=-1&activation_time=0&duration=2592000&flags=0x7&sign=${REPLY}&login=stan93_$i&passw=fail"
     resp=`curl -s -iX POST "http://${server}/oauth.html" --data "$post_data" | grep -i -o 'http://[^"]*response'`
     token=`curl -s -i "$resp" | grep -i -o 'access_token=[^"]*&user_name' | cut -c14-85`
-   echo "token$i="$token
+    #echo "token$i="$token
     #if [ "$token" = "" ];
     #  then echo "$i" >> "$fn"token_error".txt"
     #fi
